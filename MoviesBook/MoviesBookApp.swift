def7e83c5f1fb4dd03f2851d20a9404d57570645
Mehmet Ali Bunsuz <m.bunsuz1@gmail.com>
2024-07-23 16:20:53 +0300

    Initial Commit

---
+++ MoviesBook/MoviesBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesBookApp.swift
+//  MoviesBook
+//
+//  Created by Mehmet Ali Bunsuz on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
